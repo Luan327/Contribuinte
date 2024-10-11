a38a84251e581f37ef89e5d122bb4c18f6a5e0be
LuanEscritorio <luanlima747@gmail.com>
2024-10-11 14:42:49 -0300

    criado List de Tax e obtenção de dados do usuario para calculo.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import entities.TaxPayer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos contribuintes: ");
+        int quantidadeContribuinte = sc.nextInt();
+
+        List<TaxPayer> tP = new ArrayList<>();
+        for (int i = 0; i < quantidadeContribuinte; i++) {
+            System.out.println("Dados do " + (i + 1) + "º contribuinte: ");
+            System.out.print("Renda Salario: ");
+            double rendaSalario = sc.nextDouble();
+
+            System.out.print("Renda Prestação Serviço: ");
+            double rendaPrestacao = sc.nextDouble();
+
+            System.out.print("Renda Capital: ");
+            double rendaCapital = sc.nextDouble();
+
+            System.out.print("Gastos Medicos: ");
+            double gastosMedicos = sc.nextDouble();
+
+            System.out.print("Gastos educacionais: ");
+            double gastosEducacionais = sc.nextDouble();
+
+            TaxPayer taxPayer = new TaxPayer(rendaSalario,rendaPrestacao,rendaCapital,gastosMedicos,gastosEducacionais);
+            tP.add(taxPayer);
+        }
     }
 }
