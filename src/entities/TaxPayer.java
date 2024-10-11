9a230e014bb5a729a05ef0f6e03dbfea590e9da5
LuanEscritorio <luanlima747@gmail.com>
2024-10-11 16:45:22 -0300

    deixando alguns trechos de código mais simples e conciso.

--- src/entities/TaxPayer.java
+++ src/entities/TaxPayer.java
@@ -37,54 +37,42 @@ public class TaxPayer {
 
     public Double salaryTax() {
         double rendaMensal = getSalaryIncome() / 12;
-        double taxa = 0;
+
         if (rendaMensal < 3000) {
-            taxa = 0;
+           return 0.0;
         } else if (rendaMensal < 5000) {
-            taxa = getSalaryIncome() * 0.10;
+            return getSalaryIncome() * 0.10;
         } else {
-            taxa = getSalaryIncome() * 0.20;
+           return getSalaryIncome() * 0.20;
         }
-        return taxa;
     }
 
     public Double servicesTax() {
-        double taxa = 0;
-        if (getServicesIncome() > 0) {
-            taxa = getServicesIncome() * 0.15;
-        }
-        return taxa;
+        return getServicesIncome() > 0 ? getServicesIncome() * 0.15 : 0;
     }
 
     public Double capitalTax() {
-        double taxa = 0;
-        if (getCapitalIncome() > 0) {
-            taxa = getCapitalIncome() * 0.20;
-        }
-        return taxa;
+        return getCapitalIncome() > 0 ? getCapitalIncome() * 0.20 : 0;
     }
-    public Double grossTax(){
+
+    public Double grossTax() {
         return servicesTax() + capitalTax() + salaryTax();
     }
-    public double taxRebate(){
-       double impostoDesconto = grossTax() * 0.30;
-       double gastosGerais = getHealthSpending() + getEducationSpending();
-       double abatimento;
-       if(gastosGerais < impostoDesconto){
-           abatimento = gastosGerais;
-       }else{
-           abatimento = impostoDesconto;
-       }
-       return abatimento;
+
+    public double taxRebate() {
+        double impostoDesconto = grossTax() * 0.30;
+        double gastosGerais = getHealthSpending() + getEducationSpending();
+        return Math.min(gastosGerais, impostoDesconto);
     }
-    public Double netTax(){
-        return grossTax() - taxRebate();
+
+    public Double netTax() {
+        return Math.max(0,grossTax() - taxRebate());
     }
 
     @Override
     public String toString() {
-        return "Imposto bruto total: " + String.format("%.2f%n",grossTax())
-                + "Abatimento: " + String.format("%.2f%n",taxRebate())
-                + "Imposto devido: " + String.format("%.2f%n",netTax());
+        return "Imposto bruto total: " + String.format("%.2f%n", grossTax())
+                + "Abatimento: " + String.format("%.2f%n", taxRebate())
+                + "Imposto devido: " + String.format("%.2f%n", netTax());
     }
 }
