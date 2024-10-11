125201bbb4d50aa600cad164fbabb1c461e82deb
LuanEscritorio <luanlima747@gmail.com>
2024-10-11 14:32:40 -0300

    adicionado construtores e getters dos atributos

--- src/entities/TaxPayer.java
+++ src/entities/TaxPayer.java
@@ -7,6 +7,31 @@ public class TaxPayer {
     private Double healthSpending;
     private Double educationSpending;
 
+    public TaxPayer(Double salaryIncome, Double servicesIncome, Double capitalIncome, Double healthSpending, Double educationSpending) {
+        this.salaryIncome = salaryIncome;
+        this.servicesIncome = servicesIncome;
+        this.capitalIncome = capitalIncome;
+        this.healthSpending = healthSpending;
+        this.educationSpending = educationSpending;
+    }
 
+    public Double getSalaryIncome() {
+        return salaryIncome;
+    }
 
+    public Double getServicesIncome() {
+        return servicesIncome;
+    }
+
+    public Double getCapitalIncome() {
+        return capitalIncome;
+    }
+
+    public Double getHealthSpending() {
+        return healthSpending;
+    }
+
+    public Double getEducationSpending() {
+        return educationSpending;
+    }
 }
