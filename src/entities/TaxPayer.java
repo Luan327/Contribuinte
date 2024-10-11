2eea2708fb8e72ce642379b223522ab835aa481f
LuanEscritorio <luanlima747@gmail.com>
2024-10-11 16:02:51 -0300

    adicionado metodos que calculam o imposto sobre salario,capital e serviços.

--- src/entities/TaxPayer.java
+++ src/entities/TaxPayer.java
@@ -34,4 +34,36 @@ public class TaxPayer {
     public Double getEducationSpending() {
         return educationSpending;
     }
+
+    public Double salaryTax() {
+        double rendaMensal = getSalaryIncome() / 12;
+        double taxa = 0;
+        if (rendaMensal < 3000) {
+            taxa = 0;
+        } else if (rendaMensal < 5000) {
+            taxa = getSalaryIncome() * 0.10;
+        } else {
+            taxa = getSalaryIncome() * 0.20;
+        }
+        return taxa;
+    }
+
+    public Double servicesTax() {
+        double taxa = 0;
+        if (getServicesIncome() > 0) {
+            taxa = getServicesIncome() * 0.15;
+        }
+        return taxa;
+    }
+
+    public Double capitalTax() {
+        double taxa = 0;
+        if (getCapitalIncome() > 0) {
+            taxa = getCapitalIncome() * 0.20;
+        }
+        return taxa;
+    }
+    public Double grossTax(){
+        return servicesTax() + capitalTax() + salaryTax();
+    }
 }
