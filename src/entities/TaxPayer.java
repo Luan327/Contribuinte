7a8f59f748d32d0863a06a4e81ba8df53b905e51
LuanEscritorio <luanlima747@gmail.com>
2024-10-11 14:31:19 -0300

    adicionado Classe TaxPayer e atributos relacionados e encapsulados

---
+++ src/entities/TaxPayer.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class TaxPayer {
+    private Double salaryIncome;
+    private Double servicesIncome;
+    private Double capitalIncome;
+    private Double healthSpending;
+    private Double educationSpending;
+
+
+
+}
